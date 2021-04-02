1b4da2572a0d9e0cbd687bbe982e31e0c4358dd2
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-02 22:43:28 +0530

    Connecting widgetService to mySQL for performing its CRUD services.

--- src/main/java/com/example/wbdvsp2103sindhuvahinisserverjava/services/WidgetService.java
+++ src/main/java/com/example/wbdvsp2103sindhuvahinisserverjava/services/WidgetService.java
@@ -1,66 +1,69 @@
 package com.example.wbdvsp2103sindhuvahinisserverjava.services;
 
 import com.example.wbdvsp2103sindhuvahinisserverjava.models.Widget;
+import com.example.wbdvsp2103sindhuvahinisserverjava.repositories.WidgetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WidgetService {
-    private List<Widget> widgets = new ArrayList<>();
+
+    @Autowired
+    WidgetRepository repository;
 
     // Implement CRUD Operations
     public Widget createWidgetForTopic(Widget widget) {
-        Long id = (new Date()).getTime();
-        widget.setId(id);
-        widgets.add(widget);
-        return widget;
+        return repository.save(widget);
     }
 
     public List<Widget> findAllWidgets() {
-        return widgets;
+        return (List<Widget>) repository.findAll();
     }
 
     public List<Widget> findWidgetsForTopic(String topicId) {
-        List<Widget> ws = new ArrayList<>();
-        for (Widget w : widgets) {
-            if (w.getTopicId().equals(topicId)) {
-                ws.add(w);
-            }
-        }
-        return ws;
+        return repository.findWidgetsForTopic(topicId);
     }
 
+    @Nullable
     public Widget findWidgetById(Long id) {
-        for (Widget w : widgets) {
-            if (w.getId().equals(id)) {
-                return w;
-            }
-        }
-        return null;
+        Optional<Widget> widgetByIdOptional = repository.findById(id);
+        return widgetByIdOptional.orElse(null);
     }
 
     public Integer updateWidget(Long id, Widget widget) {
-        for (int i = 0; i < widgets.size(); i++) {
-            if (widgets.get(i).getId().equals(id)) {
-                widgets.set(i, widget);
-                return 1;
-            }
+        Optional<Widget> widgetByIdOptional = repository.findById(id);
+        if (widgetByIdOptional.isPresent()) {
+            Widget originalWidget = widgetByIdOptional.get();
+
+            //TODO: copy all other fields
+            originalWidget.setWidgetOrder(widget.getWidgetOrder());
+            originalWidget.setText(widget.getText());
+            originalWidget.setSrc(widget.getSrc());
+            originalWidget.setTopicId(widget.getTopicId());
+            originalWidget.setType(widget.getType());
+            originalWidget.setCssClass(widget.getCssClass());
+            originalWidget.setHeight(widget.getHeight());
+            originalWidget.setWidth(widget.getWidth());
+            originalWidget.setStyle(widget.getStyle());
+            originalWidget.setName(widget.getName());
+            originalWidget.setSize(widget.getSize());
+            originalWidget.setValue(widget.getValue());
+            originalWidget.setOrdered(widget.getOrdered());
+
+            repository.save(originalWidget);
+            return 1;
         }
         return -1;
     }
 
     public Integer deleteWidget(Long id) {
-        int index = -1;
-        for (int i = 0; i < widgets.size(); i++) {
-            if (widgets.get(i).getId().equals(id)) {
-                index = i;
-                widgets.remove(index);
-                return 1;
-            }
-        }
-        return -1;
+        repository.deleteById(id);
+        return 1;
     }
 }
