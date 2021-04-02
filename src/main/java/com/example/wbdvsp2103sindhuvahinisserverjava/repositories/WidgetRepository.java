b4a5d5451ce8b06d64cbb108fe2fa30f87994495
sindhuvahinis <somasundaram.sindhu@gmail.com>
2021-04-02 22:40:32 +0530

    creating WidgetRepository.java class

---
+++ src/main/java/com/example/wbdvsp2103sindhuvahinisserverjava/repositories/WidgetRepository.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2103sindhuvahinisserverjava.repositories;
+
+import com.example.wbdvsp2103sindhuvahinisserverjava.models.Widget;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface WidgetRepository extends CrudRepository<Widget, Long> {
+    @Query(value = "SELECT * FROM widgets WHERE topic_id=:tid", nativeQuery = true)
+    public List<Widget> findWidgetsForTopic(@Param("tid") String topicId);
+}
